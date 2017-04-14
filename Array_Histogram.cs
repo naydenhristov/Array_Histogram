b266723187afa25a70f34c978b78bad99a1c11d6
Nayden Hristov <n_hristov@mail.bg>
2017-04-14 14:42:29 +0300

    Practice Lists and Dictionaries

---
+++ Array_Histogram.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task1_4_Array_Histogram
+{
+    public class Array_Histogram
+    {
+        public static void Main()
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            List<string> inputList = Console.ReadLine()
+                .Split(' ')
+                .ToList();
+
+            int sum = 0;
+
+            foreach (string item in inputList)
+            {
+                if (words.ContainsKey(item))
+                {
+                    words[item]++;
+                    sum++;
+                }
+                else
+                {
+                    words.Add(item, 1);
+                    sum++;
+                }
+            }
+
+            var items = from pair in words
+                        orderby pair.Value descending
+                        select pair;
+
+            foreach (KeyValuePair<string, int> pair in items)
+            {
+                double percent = (double)pair.Value / sum * 100;
+                Console.WriteLine("{0} -> {1} times ({2:F2}%)", pair.Key, pair.Value, percent);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Array_Histogram
 Actually Practice Lists and Dictionaries
+
+You will be given a string array on the console (single line, space-separated). Your task is to make statistics about the elements of the array. Find out how many times each word occurs in the array. After which, sort the result by the count of occurrences in descending order and print statistics about every word in the following format:
+{word} -> {count} times ({percentage:F2}%)
+
+Exmples
+
+Input:
+hello hello pesho hello four two gosho five pesho six gosho
+
+Output:
+hello -> 3 times (27.27%)
+pesho -> 2 times (18.18%)
+gosho -> 2 times (18.18%)
+four -> 1 times (9.09%)
+two -> 1 times (9.09%)
+five -> 1 times (9.09%)
+six -> 1 times (9.09%)
+----------------------------
+
+Input:
+get the get request from the get url of get dot com
+
+Output:
+get -> 4 times (33.33%)
+the -> 2 times (16.67%)
+request -> 1 times (8.33%)
+from -> 1 times (8.33%)
+url -> 1 times (8.33%)
+of -> 1 times (8.33%)
+dot -> 1 times (8.33%)
+com -> 1 times (8.33%)
+----------------------------
+
+Input:
+trep trep trep trep trepni trepni trepni
+
+
+Output:
+trep -> 4 times (57.14%)
+trepni -> 3 times (42.86%)
+
